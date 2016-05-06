cce892cb85edc9f055c236218fb9e08237b35c77
wangk1 <kw1993oct@yahoo.com>
2016-05-06 12:49:21 -0400

    Better Constants

--- src/me/wangkev/fourtynintysix/Constants.scala
+++ src/me/wangkev/fourtynintysix/Constants.scala
@@ -4,7 +4,7 @@ package me.wangkev.fourtynintysix
  * Created by Kevin on 4/11/2016.
  */
 object Constants {
-  val STYLE_SHEET_PATH: String = "C:\\Users\\Kevin\\IdeaProjects\\2048\\src\\me\\wangkev\\fourtynintysix\\style\\style.css"
+  val STYLE_SHEET_PATH: String = "src\\me\\wangkev\\fourtynintysix\\style\\style.css"
   val GRID_SIZE:Int=4
   val GRID_MARGIN=10
   val DEFAULT_GRID_VALUE=0
